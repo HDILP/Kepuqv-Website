4c952c9fd72086cd654bdc8cc25ddb3775344977
HDILP <awm31415@163.com>
2024-05-04 18:10:14 +0800

    idk

--- post.py
+++ post.py
@@ -1,15 +1,65 @@
+# coding:utf-8
 import requests
 import re
+import json
 
-url = input('url:') 
-resp = requests.get(url)
-code = (resp.text)
-resp.close()
-code = (re.findall(r"<p>(.+?)</p>", code))
-code = (''.join(code))
-code = code.replace('<br/>','\n')
+def getPost(url):
+    # url = input('url:') 
+    # url = 'https://www.yaerxing.com/shuati/verifyShareNote?adolescent_model=0&api_key=7f7c1aa0c0658b227985268159d50a3e&api_sig=5CE39E6A6E184BE1022D22C01DD0D022&app_v=141&appid=wx2bd42ba7f4c547f5&channel=none&font_size=3&mid=12804388&nid=2726448&os_v=33&platform_id=2&rom=EMUI&timestamp=1714651507853'
+    resp = requests.get(url)
+    code = (resp.text)
+    resp.close()
 
-code = code.replace("<span class='prohibited-word'>",'')
+    post = (re.findall(r"<p>(.+?)</p>", code))
+    post = (''.join(post))
+    title = (re.findall(r'<div class="note-title">(.+?)</div>',code))
+    title = (''.join(title))
 
-code = code.replace("</span>",'')
-print(code)
+    post = post.replace('<br/>','\n')
+    post = post.replace("<span class='prohibited-word'>",'')
+    post = post.replace("</span>",'')
+
+    # print(post)
+    # print(title)
+    return post , title
+
+def getIssues():
+    url = 'https://api.github.com/repos/HDILP/Kepuqv-Website/issues'
+    # url = 'https://api.github.com/repos/HDILP/friends/issues'
+    headers = {
+        "Authorization" : "token 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",
+        "Accept" : "application/vnd.github+json",
+    }
+    response = requests.get(url, headers=headers)
+    code = response.text
+
+    code = json.loads(code)
+    # print((code))
+    code = code[0]
+    body = code["body"]
+    print(body)
+    return body
+
+url = getIssues()
+post = getPost(url)
+posts , title = post
+print(posts)
+print(title)
+'''
+---
+title: 
+description: 
+keywords: 
+categories: 
+tags:
+date: 
+headimg: 
+author: 
+---
+'''
+f = open(r'source/_posts/'+ title +'.md' , 'w' ,  encoding='utf-8')
+f.writelines('---')
+f.writelines('title: ' + title)
+f.writelines('description: ')
+# todo
+f.close()
