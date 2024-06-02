1ac287f925d0d779a9d3436bdce36f3503b114c8
HDILP <awm31415@163.com>
2024-06-02 17:15:31 +0800

    Changes to be committed: 	new file:   GetMembersAvatar.py 	modified:   source/_data/friends.yml

---
+++ GetMembersAvatar.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import json
+
+url = "https://api.yaerxing.com/GetSTBuddies"
+
+payload = "unionid=c215d6c4512b3d296f3c5f20a7055749&api_sig=74E57ACC672EC43B3419652B634CFA1F&openid=c215d6c4512b3d296f3c5f20a7055749&channel=none&type=1&app_c=144&call_id=1717310986902&os_v=33&um_token=&rom=EMUI&app_v=1.15.1&api_key=9608ebc12b0dcfac257dd071357e3c2c&appid=wx2bd42ba7f4c547f5&device_token=&platform_id=2&model=ELN-W09&page=0&brand=HONOR&search_text="
+
+headers = {
+  'User-Agent': "android",
+  'Connection': "Keep-Alive",
+  'Accept': "application/json",
+  'Accept-Encoding': "gzip",
+  'Content-Type': "application/x-www-form-urlencoded"
+}
+
+response = requests.post(url, data=payload, headers=headers)
+
+code = json.loads(response.text)
+code = code['buddies']
+
+uids = []
+logo_url = []
+
+for member in code:
+    uid = member['home_id']
+    uids.append(uid)
+    logo = member['logo']
+    logo_url.append(logo)
+
+for i in uids:
+    for url in logo_url:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # 指定保存图片的路径和文件名
+            file_path = f"./source/test/{i}.jpg"
+            
+            # 将响应内容写入文件
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+                f.close()
+
+            print(f"图片已成功保存至 {file_path}")
+            break
+        else:
+            print("图片下载失败，HTTP状态码：", response.status_code)
+            break
