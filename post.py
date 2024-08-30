e84a47c9222729b477e91565bf05e0ffde89e821
HDILP <awm31415@163.com>
2024-08-30 21:55:40 +0800

    idk

--- post.py
+++ post.py
@@ -5,54 +5,54 @@ import json
 import sys
 import time
 
+
 def getPost(url):
     # url = input('url:') 
     # url = 'https://www.yaerxing.com/shuati/verifyShareNote?adolescent_model=0&api_key=7f7c1aa0c0658b227985268159d50a3e&api_sig=5CE39E6A6E184BE1022D22C01DD0D022&app_v=141&appid=wx2bd42ba7f4c547f5&channel=none&font_size=3&mid=12804388&nid=2726448&os_v=33&platform_id=2&rom=EMUI&timestamp=1714651507853'
     resp = requests.get(url)
-    code = (resp.text)
+    code = resp.text
     resp.close()
-    FkNTFS = ['<','>','/','\\','|',':','"','*','?']
+    FkNTFS = ['<', '>', '/', '\\', '|', ':', '"', '*', '?']
     print(FkNTFS)
     post = (re.findall(r"<p>(.+?)</p>", code))
     post = (''.join(post))
     print(post)
-    if post == '': # 新文章
+    if post == '':  # 新文章
         post = (re.findall(r'<div class="note-content img-wrap">(.+?)</div>', code))
         post = (''.join(post))
 
-
-    title = (re.findall(r'<div class="note-title">(.+?)</div>',code))
-    title = (''.join(title)) 
-    title = title.replace('[' , '【')
-    title = title.replace(']' , '】')
+    title = (re.findall(r'<div class="note-title">(.+?)</div>', code))
+    title = (''.join(title))
+    title = title.replace('[', '【')
+    title = title.replace(']', '】')
     for i in FkNTFS:
-        title = title.replace(i , '') 
+        title = title.replace(i, '')
     try:
-        pictures = (re.findall(r'<img class="fill-img" src="(.*?)">',code))
+        pictures = (re.findall(r'<img class="fill-img" src="(.*?)">', code))
         print(pictures)
         headimg = pictures[0]
-        headimg = headimg.replace('http' , 'https')
+        headimg = headimg.replace('http', 'https')
     except:
-        pictures = (re.findall(r"<img src='(.*?)'>",code))
+        pictures = (re.findall(r"<img src='(.*?)'>", code))
         print(pictures)
         headimg = pictures[0]
-        headimg = headimg.replace('http','https')
-                              
-    
-    post = post.replace('<br/>','\n')
-    post = post.replace("<span class='prohibited-word'>",'')
-    post = post.replace("</span>",'')
+        headimg = headimg.replace('http', 'https')
+
+    post = post.replace('<br/>', '\n')
+    post = post.replace("<span class='prohibited-word'>", '')
+    post = post.replace("</span>", '')
 
     # print(post)
     # print(title)
-    return post , title , headimg , pictures
+    return post, title, headimg, pictures
+
 
 def getIssues():
     url = 'https://api.github.com/repos/HDILP/Kepuqv-Website/issues'
     # url = 'https://api.github.com/repos/HDILP/friends/issues'
     headers = {
-        "Authorization" : "token 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",
-        "Accept" : "application/vnd.github+json",
+        "Authorization": "token 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",
+        "Accept": "application/vnd.github+json",
     }
     response = requests.get(url, headers=headers)
     code = response.text
@@ -64,17 +64,18 @@ def getIssues():
         code = code[0]
         body = code["body"]
         print(body)
-        url1 = re.findall('<url: (.*?)>' , body)
-        author = re.findall('<author: (.*?)>' , body)
-        data = re.findall('<data: (.*?)>' , body)
+        url1 = re.findall('<url: (.*?)>', body)
+        author = re.findall('<author: (.*?)>', body)
+        data = re.findall('<data: (.*?)>', body)
         print(url1)
         url1 = url1[0]
         author = author[0]
         data = data[0]
-        return url1 , author , data
+        return url1, author, data
 
-url , author , data = getIssues()
-posts , title , headimg , pictures = getPost(url)
+
+url, author, data = getIssues()
+posts, title, headimg, pictures = getPost(url)
 
 # '''
 # ---
@@ -92,7 +93,7 @@ posts , title , headimg , pictures = getPost(url)
 # ======Front-matter======
 posttime = time.strftime('%Y-%m-%d')
 print(posttime)
-f = open(r'source/_posts/'+ title + ".md" , 'w' , encoding='utf-8')
+f = open(r'source/_posts/' + title + ".md", 'w', encoding='utf-8')
 
 # front-matter
 if data == 'yyyy-mm-dd':
@@ -100,15 +101,16 @@ if data == 'yyyy-mm-dd':
 else:
     data = data
 
-f.write('---\ntitle: ' + title + '\ndescription: \nkeywords: \ncategories: \ntags: \ndate: ' + data +'\nheadimg: '+headimg+'\nauthor: '+ author + '\n---\n\n') 
+f.write(
+    '---\ntitle: ' + title + '\ndescription: \nkeywords: \ncategories: \ntags: \ndate: ' + data + '\nheadimg: ' + headimg + '\nauthor: ' + author + '\n---\n\n')
 # ======END======
 
 # pictures
 f.write('{% gallery stretch::6::two %}\n')
 
 for i in pictures:
-    j = i.replace('http','https')
-    f.write('![](%s)\n' %j)
+    j = i.replace('http', 'https')
+    f.write('![](%s)\n' % j)
 
 f.write('{% endgallery %}\n')
 
