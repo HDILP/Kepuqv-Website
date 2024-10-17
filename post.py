767bf7859fb5274dc1adeb994af3c7bffe94a734
HDILP <awm31415@163.com>
2024-10-17 18:41:39 +0800

    update my code in my school awa

--- post.py
+++ post.py
@@ -2,9 +2,9 @@
 import requests
 import re
 import json
-import sys
+import sys, os
 import time
-
+from dotenv import load_dotenv
 
 def getPost(url):
     # url = input('url:') 
@@ -12,15 +12,12 @@ def getPost(url):
     resp = requests.get(url)
     code = resp.text
     resp.close()
-    post = (re.findall(r"<p>(.+?)</p>", code))
-    post = (''.join(post))
-    print(post)
+    post = re.findall(r"<p>(.+?)</p>", code)[0]
     if post == '':  # 新文章
         post = (re.findall(r'<div class="note-content img-wrap">(.+?)</div>', code))
         post = (''.join(post))
 
-    title = (re.findall(r'<div class="note-title">(.+?)</div>', code))
-    title = (''.join(title))
+    title = re.findall(r'<div class="note-title">(.+?)</div>', code)[0]
     title = title.replace('[', '【')
     title = title.replace(']', '】')
     FkNTFS = ['<', '>', '/', '\\', '|', ':', '"', '*', '?']
@@ -50,13 +47,18 @@ def getPost(url):
 
 
 def getIssues():
-    url = 'https://api.github.com/repos/HDILP/Kepuqv-Website/issues'
+    # 加载环境变量
+    load_dotenv()
+
+    # 从环境变量中获取 GitHub API 密钥
+    github_token = os.getenv("GITHUB_TOKEN")
+    api_url = 'https://api.github.com/repos/HDILP/Kepuqv-Website/issues'
     # url = 'https://api.github.com/repos/HDILP/friends/issues'
     headers = {
-        "Authorization": "token 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",
+        "Authorization": f"token {github_token}",
         "Accept": "application/vnd.github+json",
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(api_url, headers=headers)
     code = response.text
     if len(code) == 2:
         sys.exit(0)
@@ -66,14 +68,10 @@ def getIssues():
         code = code[0]
         body = code["body"]
         print(body)
-        url1 = re.findall('<url: (.*?)>', body)
-        author = re.findall('<author: (.*?)>', body)
-        data = re.findall('<data: (.*?)>', body)
-        print(url1)
-        url1 = url1[0]
-        author = author[0]
-        data = data[0]
-        return url1, author, data
+        post_url = re.findall('<url: (.*?)>', body)[0]
+        author = re.findall('<author: (.*?)>', body)[0]
+        data = re.findall('<data: (.*?)>', body)[0]
+        return post_url, author, data
 
 
 url, author, data = getIssues()
@@ -92,19 +90,27 @@ posts, title, headimg, pictures = getPost(url)
 # ---
 # '''
 
-# ======Front-matter======
+# ======Creat New File======
 posttime = time.strftime('%Y-%m-%d')
 print(posttime)
 f = open(r'source/_posts/' + title + ".md", 'w', encoding='utf-8')
 
-# front-matter
+# =====Front-Matter=====
 if data == 'yyyy-mm-dd':
     data = time.strftime('%Y-%m-%d')
 else:
     data = data
 
-f.write(
-    '---\ntitle: ' + title + '\ndescription: \nkeywords: \ncategories: \ntags: \ndate: ' + data + '\nheadimg: ' + headimg + '\nauthor: ' + author + '\n---\n\n')
+f.write('---\n' + \
+        'title: ' + title + \
+        '\ndescription: ' + \
+        '\nkeywords: ' + \
+        '\ncategories: ' + \
+        '\ntags: ' + \
+        '\ndate: ' + data + \
+        '\nheadimg: ' + headimg + \
+        '\nauthor: ' + author + \
+        '\n---\n\n')
 # ======END======
 
 # pictures
@@ -112,7 +118,7 @@ f.write('{% gallery stretch::6::two %}\n')
 
 for i in pictures:
     j = i.replace('http', 'https')
-    f.write('![](%s)\n' % j)
+    f.write('![](%s)\n' %j)
 
 f.write('{% endgallery %}\n')
 
