7bc777bdd93c6ab0193395d93fe8f332ac776c5e
HDILP <awm31415@163.com>
2024-10-19 11:03:26 +0800

    更新 post.py

--- post.py
+++ post.py
@@ -11,9 +11,11 @@ def getPost(url):
     resp = requests.get(url)
     code = resp.text
     resp.close()
-    post = re.findall(r"<p>(.+?)</p>", code)[0]
+    post = re.findall(r"<p>(.+?)</p>", code)
     if not post:  # 新文章
         post = re.findall(r'<div class="note-content img-wrap">(.+?)</div>', code)[0]
+    else:
+        post = post[0]
 
     title = re.findall(r'<div class="note-title">(.+?)</div>', code)[0]
     title = title.replace('[', '【')
