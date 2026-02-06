8dc168538e0c73dd640a6ee2340759a94928f510
晗菌 <awm31415@163.com>
2026-02-06 10:34:17 +0800

    quick update

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,49 @@
 const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-html-minifier-terser');
 const terser = require('gulp-terser');
-
-// 路径配置（排除库文件）
-const paths = ['./public/**/*.{css,html,js}', '!./public/{lib,lib/**}', '!./public/{libs,libs/**}', '!./public/{media,media/**}'];
-
-// 压缩 CSS
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 
+// 压缩 CSS - 使用 PostCSS + cssnano
 function minify_css() {
-  return gulp.src('./public/**/*.css', { base: './public', allowEmpty: true })
+  return gulp.src(['./public/**/*.css', '!./public/{lib,libs,media}/**'], { base: './public' })
     .pipe(postcss([
       cssnano({
         preset: ['default', {
-          discardComments: { removeAll: true },
+          discardComments: { removeAll: true }, // 移除所有注释
+          normalizeWhitespace: true            // 极度压缩空白
         }]
       })
     ]))
     .pipe(gulp.dest('./public'));
 }
 
-
-// 压缩 HTML
+// 压缩 HTML - 极致精简版
 function minify_html() {
-  return gulp.src('./public/**/*.html', { base: './public' })
+  return gulp.src(['./public/**/*.html', '!./public/{lib,libs,media}/**'], { base: './public' })
     .pipe(htmlmin({
-      collapseWhitespace: true,      // 移除空格
-      removeComments: true,          // 移除注释
-      minifyJS: true,                // 同时压缩 HTML 里的内联 JS
-      minifyCSS: true,               // 同时压缩 HTML 里的内联 CSS
-      removeAttributeQuotes: true,   // 移除属性引号（极致瘦身）
-      removeRedundantAttributes: true // 移除冗余属性
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true
     }))
     .pipe(gulp.dest('./public'));
 }
 
-// 压缩 JS (直接用 Terser 压缩现代 JS)
+// 压缩 JS - 针对现代浏览器优化
 function minify_js() {
-  return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'], { base: './public' })
+  return gulp.src(['./public/**/*.js', '!./public/**/*.min.js', '!./public/{lib,libs,media}/**'], { base: './public' })
     .pipe(terser({
       compress: {
-        drop_console: true, // 移除 console
-        drop_debugger: true,
-        passes: 2           // 压缩两次，压榨空间
+        drop_console: true, // 移除 console.log
+        passes: 2           // 运行两次压缩以获取最小体积
       },
-      mangle: true,         // 混淆变量名
       output: { comments: false }
     }))
     .pipe(gulp.dest('./public'));
 }
 
+// 并行执行
 exports.default = gulp.parallel(minify_html, minify_css, minify_js);
